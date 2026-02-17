82bb85e2551bca07a405e7a70042fc9600f6e514
mikegaravani <scotchmagico@gmail.com>
2026-02-18 00:25:41 +0100

    fix linter

--- scripts/repo-collection/fetch-swh.py
+++ scripts/repo-collection/fetch-swh.py
@@ -23,7 +23,7 @@ import time
 from dataclasses import dataclass
 from datetime import datetime, timezone
 from pathlib import Path
-from typing import Any, Dict, Iterable, List, Optional, Tuple
+from typing import Any, Dict, List, Optional, Tuple
 
 import requests
 
